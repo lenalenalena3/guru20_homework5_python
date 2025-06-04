d50716632fc81d599507473825ab7b08052f5722
Elena Tinkalyuk <lenalenalena3@mail.ru>
2025-06-04 12:35:58 +0300

    Добавлен тест test_forms_filling_and_submit_another с заполнением некоторых полей другим способом

--- conftest.py
+++ conftest.py
@@ -4,11 +4,17 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope="session")
-def open_page_demoqa():
+def setup_options():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
+
+@pytest.fixture(scope="function")
+def open_page_demoqa(setup_options):
     browser.open('https://demoqa.com/automation-practice-form')
+    browser.driver.execute_script("$('#fixedban').remove()")
+    browser.driver.execute_script("$('#footer').remove()")
     yield
     browser.quit()
+
 
